2f8c50dfbcc9c7cd1dd8f851d3bfda2af6f80f72
Rafael <rafa.cubassagra@gmail.com>
2025-02-16 17:53:48 +0100

    Automation & result storage

--- src/main/java/es/uma/informatica/misia/ae/simpleea/EvolutionaryAlgorithm.java
+++ src/main/java/es/uma/informatica/misia/ae/simpleea/EvolutionaryAlgorithm.java
@@ -1,67 +1,181 @@
 package es.uma.informatica.misia.ae.simpleea;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class EvolutionaryAlgorithm {
-	public static final String MAX_FUNCTION_EVALUATIONS_PARAM = "maxFunctionEvaluations";
-	public static final String RANDOM_SEED_PARAM = "randomSeed";
 	public static final String POPULATION_SIZE_PARAM = "populationSize";
-	
+	public static final String MAX_FUNCTION_EVALUATIONS_PARAM = "maxFunctionEvaluations";
+	public static final String CROSSOVER_PROBABILITY_PARAM = "crossoverprob";
+	public static final String PROBLEM_SIZE_PARAM = "problemsize";
+	public static final String EARLYSTOP_PARAM = "earlystop";
+	public static final String RANDOM_SEED_PARAM = "randomSeed";
+
+	private int populationSize;
+	private int maxFunctionEvaluations;
+	private double crossoverprob;
+	private double bitFlipProb;
+	private int problemsize;
+	private Boolean earlystop;
+	private long randomSeed;
+
 	private Problem problem;
 	private int functionEvaluations;
-	private int maxFunctionEvaluations;
 	private List<Individual> population;
-	private int populationSize;
+
 	private Random rnd;
-	
 	private Individual bestSolution;
-	
+
 	private Selection selection;
 	private Replacement replacement;
 	private Mutation mutation;
 	private Crossover recombination;
-	
+
+	private FileWriter csvWriter;
+
 	public EvolutionaryAlgorithm(Map<String, Double> parameters, Problem problem) {
 		configureAlgorithm(parameters, problem);
+		initializeCsvWriter();
 	}
-	
+
+	private void initializeCsvWriter() {
+		String directoryName = String.format("Data/ProS_%d/Es_%b/BITFLIP_%.2f_CROSSOVER_%.2f/", problemsize, earlystop, bitFlipProb, crossoverprob);
+		File directory = new File(directoryName);
+		if (!directory.exists()) {
+			directory.mkdirs();
+		}
+		String fileName = String.format("randomSeed_%d.csv", randomSeed);
+		File csvFile = new File(directory, fileName);
+		try {
+			csvWriter = new FileWriter(csvFile);
+			csvWriter.append("Evaluations;BestFitness;MeanFitness;Diversity;UniqueIndividuals;PopulationSize;ProblemSize;BitFlipProbability;CrossoverProbability;RandomSeed\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void configureAlgorithm(Map<String, Double> parameters, Problem problem) {
 		populationSize = parameters.get(POPULATION_SIZE_PARAM).intValue();
 		maxFunctionEvaluations = parameters.get(MAX_FUNCTION_EVALUATIONS_PARAM).intValue();
-		double bitFlipProb = parameters.get(BitFlipMutation.BIT_FLIP_PROBABILITY_PARAM);
-		long randomSeed = parameters.get(RANDOM_SEED_PARAM).longValue();
-		
-		this.problem = problem; 
-		
+		bitFlipProb = parameters.get(BitFlipMutation.BIT_FLIP_PROBABILITY_PARAM);
+		crossoverprob = parameters.get(CROSSOVER_PROBABILITY_PARAM);
+		problemsize = parameters.get(PROBLEM_SIZE_PARAM).intValue();
+		earlystop = (parameters.get(EARLYSTOP_PARAM) != 0.0);
+		randomSeed = parameters.get(RANDOM_SEED_PARAM).longValue();
+		this.problem = problem;
 		rnd = new Random(randomSeed);
-		
 		selection = new BinaryTournament(rnd);
 		replacement = new ElitistReplacement();
 		mutation = new BitFlipMutation(rnd, bitFlipProb);
 		recombination = new SinglePointCrossover(rnd);
 	}
-	
+
 	public Individual run() {
 		population = generateInitialPopulation();
 		functionEvaluations = 0;
-		
 		evaluatePopulation(population);
 		while (functionEvaluations < maxFunctionEvaluations) {
 			Individual parent1 = selection.selectParent(population);
 			Individual parent2 = selection.selectParent(population);
-			Individual child = recombination.apply(parent1, parent2);
+			Individual child;
+			if (Math.random() <= crossoverprob) {
+				child = recombination.apply(parent1, parent2);
+			} else {
+				child = parent1;
+			}
 			child = mutation.apply(child);
 			evaluateIndividual(child);
 			population = replacement.replacement(population, Arrays.asList(child));
+			recordStatistics();
+			if (earlystop && child.getFitness() == problemsize) {
+				break;
+			}
 		}
-		
+		try {
+			csvWriter.flush();
+			csvWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return bestSolution;
 	}
-	
+
+	private void recordStatistics() {
+		double meanFitness = calculateMeanFitness();
+		double diversity = calculateDiversity();
+		int uniqueIndividuals = calculateUniqueIndividuals();
+		try {
+			csvWriter.append(String.format("%d;%d;%.4f;%.4f;%d;%d;%d;%.2f;%.2f;%d\n",
+					functionEvaluations,
+					(int) bestSolution.getFitness(),
+					meanFitness,
+					diversity,
+					uniqueIndividuals,
+					populationSize,
+					problemsize,
+					bitFlipProb,
+					crossoverprob,
+					randomSeed));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private double calculateMeanFitness() {
+		double totalFitness = 0.0;
+		for (Individual individual : population) {
+			totalFitness += individual.getFitness();
+		}
+		return totalFitness / population.size();
+	}
+
+	private double calculateDiversity() {
+		double totalDistance = 0.0;
+		int comparisons = 0;
+		for (int i = 0; i < population.size(); i++) {
+			BinaryString individualA = (BinaryString) population.get(i);
+			byte[] chromosomeA = individualA.getChromosome();
+			for (int j = i + 1; j < population.size(); j++) {
+				BinaryString individualB = (BinaryString) population.get(j);
+				byte[] chromosomeB = individualB.getChromosome();
+				totalDistance += hammingDistance(chromosomeA, chromosomeB);
+				comparisons++;
+			}
+		}
+		return comparisons > 0 ? totalDistance / comparisons : 0.0;
+	}
+
+	private int calculateUniqueIndividuals() {
+		Set<String> uniqueIndividuals = new HashSet<>();
+		for (Individual individual : population) {
+			BinaryString binaryIndividual = (BinaryString) individual;
+			byte[] chromosome = binaryIndividual.getChromosome();
+			uniqueIndividuals.add(Arrays.toString(chromosome));
+		}
+		return uniqueIndividuals.size();
+	}
+
+	private int hammingDistance(byte[] a, byte[] b) {
+		int dist = 0;
+		int len = a.length;
+		int i = 0;
+		int limit = len - (len % 4);
+		for (; i < limit; i += 4) {
+			if (a[i] != b[i]) dist++;
+			if (a[i + 1] != b[i + 1]) dist++;
+			if (a[i + 2] != b[i + 2]) dist++;
+			if (a[i + 3] != b[i + 3]) dist++;
+		}
+		for (; i < len; i++) {
+			if (a[i] != b[i]) {
+				dist++;
+			}
+		}
+		return dist;
+	}
+
 	private void evaluateIndividual(Individual individual) {
 		double fitness = problem.evaluate(individual);
 		individual.setFitness(fitness);
@@ -70,23 +184,22 @@ public class EvolutionaryAlgorithm {
 	}
 
 	private void checkIfBest(Individual individual) {
-		if (bestSolution == null || individual.getFitness()> bestSolution.getFitness()) {
+		if (bestSolution == null || individual.getFitness() > bestSolution.getFitness()) {
 			bestSolution = individual;
 		}
 	}
 
 	private void evaluatePopulation(List<Individual> population) {
-		for (Individual individual: population) {
+		for (Individual individual : population) {
 			evaluateIndividual(individual);
 		}
 	}
 
 	private List<Individual> generateInitialPopulation() {
 		List<Individual> population = new ArrayList<>();
-		for (int i=0; i < populationSize; i++) {
+		for (int i = 0; i < populationSize; i++) {
 			population.add(problem.generateRandomIndividual(rnd));
 		}
 		return population;
 	}
-	
 }
--- src/main/java/es/uma/informatica/misia/ae/simpleea/Main.java
+++ src/main/java/es/uma/informatica/misia/ae/simpleea/Main.java
@@ -1,40 +1,110 @@
 package es.uma.informatica.misia.ae.simpleea;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class Main {
-
-	public static void main (String args []) {
-		
-		if (args.length < 4) {
+	public static void main(String args[]) {
+		if (args.length != 2) {
 			System.err.println("Invalid number of arguments");
-			System.err.println("Arguments: <population size> <function evaluations> <bitflip probability> <problem size> [<random seed>]");
+			System.err.println("Arguments: <population size> <function evaluations>");
 			return;
 		}
-		
-		int n = Integer.parseInt(args[3]);
-		Problem problem = new Onemax(n);
-		
-		Map<String, Double> parameters = readEAParameters(args);
-		EvolutionaryAlgorithm evolutionaryAlgorithm = new EvolutionaryAlgorithm(parameters, problem);
-		
-		Individual bestSolution = evolutionaryAlgorithm.run();
-		System.out.println(bestSolution);
+		String folderPath = "Data";
+		File folder = new File(folderPath);
+		if (deleteFolder(folder)) {
+			System.out.println("The folder was successfully deleted.");
+		} else {
+			System.out.println("The folder could not be deleted. Please check the path or permissions.");
+		}
+		int numSeeds = 100;
+		long totalTasks = numSeeds * 2 * 6 * 5 * 2;
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		ScheduledExecutorService progressMonitor = Executors.newSingleThreadScheduledExecutor();
+		final long startTime = System.currentTimeMillis();
+		progressMonitor.scheduleAtFixedRate(() -> {
+			if (executor instanceof ThreadPoolExecutor) {
+				ThreadPoolExecutor tpe = (ThreadPoolExecutor) executor;
+				long completed = tpe.getCompletedTaskCount();
+				double progressPercent = 100.0 * completed / totalTasks;
+				int progressChars = (int) (progressPercent / 2);
+				String progressBar = "#".repeat(progressChars) + " ".repeat(50 - progressChars);
+				long elapsedMillis = System.currentTimeMillis() - startTime;
+				String elapsedTime = formatTime(elapsedMillis);
+				String remainingTime = "Computing...";
+				if (completed > 0) {
+					double avgTaskTime = (double) elapsedMillis / completed;
+					long remainingMillis = (long) (avgTaskTime * (totalTasks - completed));
+					remainingTime = formatTime(remainingMillis);
+				}
+				System.out.print("\r[" + progressBar + "] " + String.format("%.2f", progressPercent)
+						+ "% (" + completed + "/" + totalTasks + " tasks) - Elapsed: " + elapsedTime
+						+ " | Remaining: " + remainingTime);
+			}
+		}, 0, 1, TimeUnit.SECONDS);
+		for (int seed = 1; seed < numSeeds; seed++) {
+			for (double problemsize : new double[]{25.0, 50.0}) {
+				for (double bitFlipProb = 0.05; bitFlipProb <= 0.30; bitFlipProb += 0.05) {
+					for (double crossoverProb = 0; crossoverProb <= 1; crossoverProb += 0.25) {
+						for (double earlystop : new double[]{0.0, 1.0}) {
+							final int seedFinal = seed;
+							final double bitFlipProbFinal = bitFlipProb;
+							final double crossoverProbFinal = crossoverProb;
+							executor.submit(() -> {
+								Map<String, Double> parameters = readEAParameters(args, seedFinal, bitFlipProbFinal, crossoverProbFinal);
+								parameters.put(EvolutionaryAlgorithm.PROBLEM_SIZE_PARAM, problemsize);
+								parameters.put(EvolutionaryAlgorithm.EARLYSTOP_PARAM, earlystop);
+								Problem problem = new Onemax((int) problemsize);
+								EvolutionaryAlgorithm evolutionaryAlgorithm = new EvolutionaryAlgorithm(parameters, problem);
+								evolutionaryAlgorithm.run();
+							});
+						}
+					}
+				}
+			}
+		}
+		executor.shutdown();
+		try {
+			if (!executor.awaitTermination(32, TimeUnit.HOURS)) {
+				System.err.println("Not all tasks completed in the expected time.");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		progressMonitor.shutdown();
+		System.out.println("\nAll tasks have finished!");
 	}
 
-	private static Map<String, Double> readEAParameters(String[] args) {
+	public static boolean deleteFolder(File folder) {
+		if (!folder.exists()) return false;
+		if (folder.isDirectory()) {
+			File[] files = folder.listFiles();
+			if (files != null) {
+				for (File file : files) {
+					deleteFolder(file);
+				}
+			}
+		}
+		return folder.delete();
+	}
+
+	private static Map<String, Double> readEAParameters(String[] args, int seed, double bitFlipProb, double crossoverProb) {
 		Map<String, Double> parameters = new HashMap<>();
 		parameters.put(EvolutionaryAlgorithm.POPULATION_SIZE_PARAM, Double.parseDouble(args[0]));
 		parameters.put(EvolutionaryAlgorithm.MAX_FUNCTION_EVALUATIONS_PARAM, Double.parseDouble(args[1]));
-		parameters.put(BitFlipMutation.BIT_FLIP_PROBABILITY_PARAM, Double.parseDouble(args[2]));
-		
-		long randomSeed = System.currentTimeMillis();
-		if (args.length > 4) {
-			randomSeed = Long.parseLong(args[4]);
-		}
-		parameters.put(EvolutionaryAlgorithm.RANDOM_SEED_PARAM, (double)randomSeed);
+		parameters.put(BitFlipMutation.BIT_FLIP_PROBABILITY_PARAM, bitFlipProb);
+		parameters.put(EvolutionaryAlgorithm.CROSSOVER_PROBABILITY_PARAM, crossoverProb);
+		parameters.put(EvolutionaryAlgorithm.RANDOM_SEED_PARAM, (double) seed);
 		return parameters;
 	}
 
+	private static String formatTime(long millis) {
+		long seconds = millis / 1000;
+		long s = seconds % 60;
+		long m = (seconds / 60) % 60;
+		long h = seconds / 3600;
+		return String.format("%02d:%02d:%02d", h, m, s);
+	}
 }
